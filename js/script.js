a2721ce8115666bbff18a0124819982ee85a3edc
i0hafsa0i-lang <i0.hafsa.0i@gmail.com>
2026-08-05 19:17:27 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 console.log("Website Loaded");
+// Load existing cart
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Update cart count
+function updateCartCount() {
+    const cartCount = document.getElementById("cart-count");
+
+    if (cartCount) {
+        cartCount.textContent = cart.length;
+    }
+}
+
+updateCartCount();
+
+// Add product
+const buttons = document.querySelectorAll(".add-to-cart");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const product = {
+            name: button.dataset.name,
+            price: Number(button.dataset.price)
+        };
+
+        cart.push(product);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        updateCartCount();
+
+        alert(product.name + " added to cart!");
+    });
+
+});
